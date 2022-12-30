920dd41b0e10e1f7455c569c991a3209d42f039b
Yablokova Irina <yablokovairinav@yandex.ru>
2022-12-30 20:07:34 +0300

    With no room for error

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# api_final
-api final
+API для проекта Yatube
+Yatube - социальная сеть для блогеров. Неутентифицированным пользователям API доступен только на чтение. Исключение - эндпоинт /follow/: доступ к нему предоставляется только аутентифицированным пользователям. В проекте использована аутентификация по JWT-токенам. Аутентифицированным пользователям разрешено изменение и удаление своего контента. Добавление новых пользователей через API не осуществляется.
+
+Как запустить проект:
+Клонировать репозиторий и перейти в него в командной строке:
+
+git clone https://github.com/YablokovaIrina/api_final_yatube.git
+cd api_final_yatube
+Cоздать и активировать виртуальное окружение:
+
+python3 -m venv env
+source env/bin/activate
+Установить зависимости из файла requirements.txt:
+
+python3 -m pip install --upgrade pip
+pip install -r requirements.txt
+Выполнить миграции:
+
+python3 manage.py migrate
+Запустить проект:
+
+python3 manage.py runserver
+Примеры некоторых запросов к API:
+GET http://127.0.0.1:8000/api/v1/posts/
+Вернёт:
+
+{
+    "count": 123,
+    "next": "http://api.example.org/accounts/?offset=400&limit=100",
+    "previous": "http://api.example.org/accounts/?offset=200&limit=100",
+    "results":[
+        +{...}
+    ]
+}
+POST http://127.0.0.1:8000/api/v1/follow/
+{
+    "following": "string"
+}
+Вернёт:
+
+{
+    "user": "string",
+    "following": "string"
+}
+
+Об авторе:
+Ирина Яблокова
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
+
+from posts.models import Comment, Post, Group, Follow, User
 
 
-from posts.models import Comment, Post
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -21,3 +27,29 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post',)
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault())
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all())
+    
+    def validate_following(self, value):
+        if self.context['request'].user == value:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя!')
+        return value
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following'],
+            )]
